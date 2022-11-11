18bbb742e162ce71cd1d3ab05185cc08ecd25866
Björn Strausmann <bjoern@strausmann.net>
2022-11-11 12:47:17 +0000

    #14 Add Multi Arch Build Process

--- build.sh
+++
@@ -1,5 +0,0 @@
-#docker build -t flask-local-build --build-arg GIT_COMMIT=$(git log -1 --format=%h) .
-
-BRC_VERSION=$(curl --silent "https://api.github.com/repos/Pugmatt/BedrockConnect/releases/latest" | jq -r .tag_name)
-
-DOCKER_BUILDKIT=1  docker build -t strausmann/minecraft-bedrock-connect .  --build-arg BRC_VERSION=$BRC_VERSION --label "org.opencontainers.image.revision=$(git rev-parse --short HEAD)" --label "org.opencontainers.image.created=$(date -I)"
