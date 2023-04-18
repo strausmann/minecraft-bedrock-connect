0c536a633f266756ff6b10b3160e17a3c92f52b0
Björn Strausmann <bjoern@strausmann.net>
2023-04-18 16:57:09 +0200

    Resolve "Extracting jar file for brc_version >= 1.30 fails"

--- image/bedrock-entry.sh
+++ image/bedrock-entry.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+version_greater_equal()
+{
+    printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
+}
+
 set -eo pipefail
 
 : "${TMP_DIR:=/tmp}"
@@ -11,6 +16,7 @@ if [[ ${DEBUG^^} = TRUE ]]; then
   echo "       current directory is $(pwd)"
 fi
 
+brc_source_file="BedrockConnect-1.0-SNAPSHOT.jar"
 mysql_host=${MYSQL_HOST:-localhost}
 mysql_db=${MYSQL_DB:-bedrock-connect}
 mysql_user=${MYSQL_USER:-bedrock}
@@ -31,14 +37,20 @@ java_xmx=${JAVA_XMX:-256M}
 [[ ! -z "$CUSTOM_SERVERS" ]] && custom_servers="custom_servers=${CUSTOM_SERVERS}" && echo "custom_servers set to ${CUSTOM_SERVERS}" || echo "custom_servers disabled"
 
 if [ "$brc_version" = "latest" ]; then
-    BRC_VERSION=$(curl --silent "https://api.github.com/repos/Pugmatt/BedrockConnect/releases/latest" | jq -r .tag_name)
-    echo "Latest Bedrock Connect version is: $BRC_VERSION"
+    brc_version=$(curl --silent "https://api.github.com/repos/Pugmatt/BedrockConnect/releases/latest" | jq -r .tag_name)
+    echo "Latest Bedrock Connect version is: ${brc_version}"
 fi
 
-echo "Downloading now Bedrock Connect JAR"
-easy-add --var version=$BRC_VERSION --from https://github.com/Pugmatt/BedrockConnect/releases/download/{{.version}}/BedrockConnect-setup.zip --file BedrockConnect-1.0-SNAPSHOT.jar -to /docker/brc
+# #20 Fix since BRC 1.30
+if (version_greater_equal "${brc_version}" 1.30 ); then
+    brc_source_file="BedrockConnect-setup/BedrockConnect-1.0-SNAPSHOT.jar"
+    echo "Bedrock Connect Jar Source File: '${brc_source_file}'"
+fi
+
+echo "Downloading now Bedrock Connect JAR - version: $brc_version"
+easy-add --var version=$brc_version --from https://github.com/Pugmatt/BedrockConnect/releases/download/{{.version}}/BedrockConnect-setup.zip --file $brc_source_file -to /docker/brc
 chmod 664 /docker/brc/BedrockConnect-1.0-SNAPSHOT.jar
 
-echo "Bedrock Connect Version: ${BRC_VERSION}"
+echo "Bedrock Connect version: ${brc_version}"
 
 java -Xms${java_xms} -Xmx${java_xmx} -jar /docker/brc/BedrockConnect-1.0-SNAPSHOT.jar server_limit=${server_limit} mysql_host=${mysql_host} mysql_db=${mysql_db} mysql_user=$mysql_user mysql_pass=$mysql_pass kick_inactive=$kick_inactive user_servers=$user_servers featured_servers=$featured_servers nodb=$nodb $custom_servers fetch_featured_ips=$fetch_featured_ips ${whitelist}
