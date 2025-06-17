d0516542b7614447721a893b56e14d0d097608be
Björn Strausmann <bjoern@strausmann.net>
2025-06-17 09:51:43 +0000

    feat(env): Add new ENV Vars MOTD and DEBUG by BedrockConnect Release v1.56

--- image/bedrock-entry.sh
+++ image/bedrock-entry.sh
@@ -18,7 +18,7 @@ mysql_host=${MYSQL_HOST:-localhost}
 mysql_db=${MYSQL_DB:-bedrock-connect}
 mysql_user=${MYSQL_USER:-bedrock}
 mysql_pass=${MYSQL_PASS:-bedrock}
-db_type=${DB_TYPE:-mysql}
+db_type=${DB_TYPE:-none}
 db_host=${DB_HOST:-$MYSQL_HOST}
 db_db=${DB_DB:-$MYSQL_DB}
 db_user=${DB_USER:-$MYSQL_USER}
@@ -33,12 +33,16 @@ user_servers=${USER_SERVERS:-true}
 featured_servers=${FEATURED_SERVERS:-true}
 fetch_featured_ips=${FETCH_FEATURED_IPS:-true}
 fetch_ips=${FETCH_IPS:-false}
+debug=${DEBUG:-true}
+motd_first_join=${MOTD_FIRST_JOIN:-true}
+motd_cooldown=${MOTD_COOLDOWN:-0}
 store_display_names=${STORE_DISPLAY_NAMES:-true}
 packet_limit=${PACKET_LIMIT:-200}
 global_packet_limit=${GLOBAL_PACKET_LIMIT:-100000}
 java_xms=${JAVA_XMS:-256M}
 java_xmx=${JAVA_XMX:-256M}
 
+[[ ! -z "$MOTD" ]] && motd="whitelist=${MOTD}" && echo "whitelist set to ${MOTD}" || echo "motd disabled"
 [[ ! -z "$WHITELIST" ]] && whitelist="whitelist=${WHITELIST}" && echo "whitelist set to ${WHITELIST}" || echo "whitelist disabled"
 [[ ! -z "$LANGUAGE" ]] && language="language=${LANGUAGE}" && echo "language set to ${LANGUAGE}" || echo "language disabled"
 [[ ! -z "$CUSTOM_SERVERS" ]] && custom_servers="custom_servers=${CUSTOM_SERVERS}" && echo "custom_servers set to ${CUSTOM_SERVERS}" || echo "custom_servers disabled"
@@ -69,10 +73,14 @@ java -Xms${java_xms} -Xmx${java_xmx} -jar /docker/brc/BedrockConnect-1.0-SNAPSHO
     user_servers=${user_servers} \
     featured_servers=${featured_servers} \
     fetch_featured_ips=${fetch_featured_ips} \
+    debug=${debug} \
+    motd_first_join=${motd_first_join} \
+    motd_cooldown=${motd_cooldown} \
     store_display_names=${store_display_names} \
     packet_limit=${packet_limit} \
     global_packet_limit=${global_packet_limit} \
     nodb=${nodb} \
+    ${motd} \
     ${whitelist} \
     ${language} \
     ${custom_servers} \
